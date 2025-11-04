de3c00ce79fda9ef4514feac12743f3c855bbc42
Tulio Dias <tuliodias@gmail.com>
2025-11-04 18:05:06 -0300

    version 1.0.0

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const header = document.getElementById('header');
 const about = document.getElementById('about');
 const areas = document.getElementById('areas');
 const footer = document.getElementById('footer');
+const toggle = document.getElementById('menu__toggle');
 
 cards.forEach(card =>
   card.addEventListener('click', function (e) {
@@ -12,3 +13,10 @@ cards.forEach(card =>
     card.classList.toggle('areas__card-flip');
   })
 );
+
+toggle.addEventListener('click', function (e) {
+  e.preventDefault();
+  const toggleClass = document.querySelector('.popup');
+
+  toggleClass.classList.toggle('visible');
+});
